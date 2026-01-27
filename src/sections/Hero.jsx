ae09577d416d5f143bc69414d4a6d14711073045
Saptarshi Upadhyay <upadhyay.saptarshi@gmail.com>
2026-01-27 22:44:59 +0530

    Hero_Changes

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,12 @@
 export const Hero=()=>{
     return (
     <section className="relative min-h-screen flex items-center overflow-hidden">
-        <div>
-            <img/>
+        <div className="absolute inset-0">
+            <img src="/b1.jpg" alt="Hero Image" className="w-full h-full object-cover opacity-35 "/>
+            <div className="absolute inset-0 bg-gradient-to-b 
+  from-[rgb(15,20,24)/0.4] 
+  via-[rgb(15,20,24)/0.9] 
+  to-[rgb(15,20,24)]"/>
         </div>
     </section>
     )
