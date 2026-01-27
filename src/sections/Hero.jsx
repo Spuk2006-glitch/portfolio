64b0431aa883af81c87df21692c9d304f4dda1e1
Saptarshi Upadhyay <upadhyay.saptarshi@gmail.com>
2026-01-27 23:36:22 +0530

    Bg_fixing

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,12 +2,32 @@ export const Hero=()=>{
     return (
     <section className="relative min-h-screen flex items-center overflow-hidden">
         <div className="absolute inset-0">
-            <img src="/b1.jpg" alt="Hero Image" className="w-full h-full object-cover opacity-35 "/>
-            <div className="absolute inset-0 bg-gradient-to-b 
-  from-[rgb(15,20,24)/0.4] 
-  via-[rgb(15,20,24)/0.9] 
-  to-[rgb(15,20,24)]"/>
+            <img src="/b1.jpg" alt="Hero Image" className="w-full h-full object-cover opacity-40 "/>
+            <div className="absolute inset-0 bg-gradient-to-b from-background/20 via-background/80 to-background"/>
         </div>
+        
+        <div className="absolute inset-0 overflow-hidden pointer-events-none">
+            {[...Array(30)].map((_, i) => (
+          <div
+            className="absolute w-1.5 h-1.5 rounded-full opacity-20"
+            style={{
+              backgroundColor: "#20B2A6",
+              left: `${Math.random() * 100}%`,
+              top: `${Math.random() * 100}%`,
+              animation:`slow-drift ${
+                15+Math.random()*20
+              }s ease-in-out infinite`,
+              animationDelay:`${Math.random()*5}s`
+            }}
+          />
+        ))}
+        </div>
+
+        {/* <div>
+            <div>
+
+            </div>
+        </div> */}
     </section>
     )
 }
