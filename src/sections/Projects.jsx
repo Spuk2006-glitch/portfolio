6e225eb843de662a519b2df75ad164e71a4e3b7e
Saptarshi Upadhyay <upadhyay.saptarshi@gmail.com>
2026-02-03 00:36:26 +0530

    Project

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -116,10 +116,16 @@ export const Projects=()=>{
 
         {/* View All CTA */}
         <div className="text-center mt-12 animate-fade-in animation-delay-500">
-          <AnimatedBorderButton>
-            View All Projects
-            <ArrowUpRight className="w-5 h-5" />
-          </AnimatedBorderButton>
+            <a
+            href="https://github.com/Spuk2006-glitch"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            <AnimatedBorderButton>
+                View All Projects
+                <ArrowUpRight className="w-5 h-5" />
+            </AnimatedBorderButton>
+            </a>
         </div>
       </div>
     </section>
